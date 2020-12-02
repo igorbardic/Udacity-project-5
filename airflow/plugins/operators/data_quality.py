8dfff165143e2856ff752ebd7eea06e10ae67ceb
Igor Bardić <73275227+igorbardic@users.noreply.github.com>
2020-12-02 07:42:14 +0100

    Airflow project
    Final

---
+++ airflow/plugins/operators/data_quality.py
@@ -0,0 +1,43 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class DataQualityOperator(BaseOperator):
+
+    ui_color = '#89DA59'
+
+    @apply_defaults
+    def __init__(self,
+                 # Define your operators params (with defaults) here
+                 # Example:
+                 # conn_id = your-connection-name
+                 *args, **kwargs):
+
+        super(DataQualityOperator, self).__init__(*args, **kwargs)
+        # Map params here
+        # Example:
+        # self.conn_id = conn_id
+
+    def execute(self, context):
+        self.log.info('DataQualityOperator not implemented yet')
+        
+    def __init__(self,
+                 redshift_id='',
+                 table_name='',
+                 *args, **kwargs):
+
+        super(DataQualityOperator, self).__init__(*args, **kwargs)
+        self.redshift_id=redshift_id
+        self.table_name=table_name
+
+    def execute(self, context):
+        redshift=PostgresHook(self.redshift_id)
+        sql="SELECT COUNT(*) FROM {tablename}".format(tablename=self.table_name)
+        records=redshift.get_records(sql)
+        self.log.info('Testing.')
+        if len(records) < 1 or len(records[0]) < 1:
+            raise ValueError(f"Data quality check failed. {self.table_name} has no values")
+        num_records = records[0][0]
+        if num_records < 1:
+            raise ValueError(f"Data quality check failed. {self.table_name} contained 0 rows")
+        self.log.info(f"Data quality on table {self.table_name} check passed with {records[0][0]} records")        
---
+++ airflow/plugins/operators/load_fact.py
@@ -0,0 +1,30 @@
+from airflow.hooks.postgres_hook import PostgresHook
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+class LoadFactOperator(BaseOperator):
+
+    ui_color = '#F98866'
+
+    @apply_defaults
+    def __init__(self,
+                 redshift_id='',
+                 sql='',
+                 mode='',
+                 *args, **kwargs):
+
+        super(LoadFactOperator, self).__init__(*args, **kwargs)
+        self.redshift_id=redshift_id
+        self.sql=sql
+        self.mode=mode
+
+    def execute(self, context):
+        self.log.info('Setting.')
+        redshift=PostgresHook(postgres_conn_id=self.redshift_id)
+        
+        self.log.info('Loading dimension table.')
+        if self.mode == 'insert':
+            redshift.run("INSERT INTO songplays " + self.sql)
+        else:
+            redshift.run('DELETE FROM songplays')
+            redshift.run("INSERT INTO songplays " + self.sql)
